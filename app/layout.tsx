491c0b87bbdeed4d3dc5475a9d103b99c4b905ff
hidayatur90 <hidabaik@gmail.com>
2024-03-15 22:01:35 +0700

    chap16 : adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | HD Dashboard',
+    default: 'HD Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://hd-nextjs-iota.vercel.app/'),
+};
 
 export default function RootLayout({
   children,
