5bf920f501f41839028a36347fddee8fef9098ce
hidayatur90 <hidabaik@gmail.com>
2024-03-17 21:03:55 +0700

    table customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,24 @@
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers, fetchInvoicesPages } from '@/app/lib/data';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
     title: 'Customers',
 };
 
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+    };
+}) {
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);
 
-export default function Page(){
-    return <p>Customers Page</p>;
+    return (
+        <div className="w-full">
+            <Table customers={customers} />
+        </div>
+    );
 }
